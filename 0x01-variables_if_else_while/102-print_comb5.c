9d4611d5cc772d5fb97662516cf65911db1bcbac
Brenden <mylgv40b@gmail.com>
2022-06-03 22:59:17 +0200

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i, e, g, h, op1, op2;
 		i = e = g = h = 48;
-					while (h < 58)
+				while (h < 58)
 	{
 		g = 48;
-					while (g < 58)
+				while (g < 58)
 	{
 			e = 48;
-					while (e < 58)
+				while (e < 58)
 		{
 				i = 48;
 					while (i < 58)
