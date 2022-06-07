356fa9a781f3a35c9308fb0c304d9159741747e8
Brenden <mylgv40b@gmail.com>
2022-06-07 19:57:15 +0200

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - Print sign of a numb.
+* Return: 1 and prints + if n is greater than zero
+* Return: 0 and prints 0 if n is zero
+* Return: -1 and prints - if n is less than zero
+*/
+
+int print_sign(int n)
+{
+		if (n > 0)
+	{
+			_putchar('+');
+		return (1);
+	}
+		else if (n == o)
+		{
+			_putchar('0');
+		return (0);
+	else
+	_putchar('-');
+	return (-1);
+		}
+/*bN*/
+}
