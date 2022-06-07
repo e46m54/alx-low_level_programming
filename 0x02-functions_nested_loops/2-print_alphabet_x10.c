f9cefb19875169419188766cdd5efb7f7626f055
Brenden <mylgv40b@gmail.com>
2022-06-07 18:33:06 +0200

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet x10 - Prints alphabet in lowercase 10 times.
+*
+**/
+void print_alphabet_x10(void);
+{
+			char c;
+
+	for (c = 'a'; c <= 'z'; c++) * 10;
+
+	{
+	_putchar(c);
+	}
+
+	_putchar('\n');
+}
