905d7178d746ea8a75e870016460eb1345142f44
Brenden <mylgv40b@gmail.com>
2022-06-07 20:09:04 +0200

    does this even work?

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* int _abs - calculate absulute of int
+* @n: input an integer
+* Return: Absolute value of int
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+		else
+	{
+		return (n);
+	}
+/*bN*/
+}
