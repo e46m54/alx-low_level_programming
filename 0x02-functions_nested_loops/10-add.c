3192ce3837387aa7bf2139cd62660e38fcf035c8
Brenden <mylgv40b@gmail.com>
2022-06-07 21:11:24 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "mian.h"
+/*
+* add - add int
+* @a: int added to b
+* @b: int added to a
+* Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+/*bN*/
+}
