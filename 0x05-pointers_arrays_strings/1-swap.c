f1bbecf277699c8d7e89e3692301a37ae210d054
e46m54 <mylgv40b@gmail.com>
2022-06-13 17:55:42 +0200

    tryout #1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - Swap the int
+* @a: pointer to int
+* @b: pointer to int
+* Return: void
+*/
+void swap_int(int *a, int *b);
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+
+}
