1f30f1a0e281548411b6052555ae7d1d6c0c3ee0
e46m54 <mylgv40b@gmail.com>
2022-06-13 19:07:45 +0200

    try

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts - a function that prints a string
+* @str: point to string
+* Return: void
+*/
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		i++;
+		str++;
+	}
+	_putchar('\n')
+}
