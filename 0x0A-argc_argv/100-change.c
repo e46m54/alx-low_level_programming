d9614b7830954112e46d2ea29a7e6000b74fdae5
e46m54 <mylgv40b@gmail.com>
2022-06-24 17:29:00 +0200

    try

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins to make change for an amount of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, ncoins = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
