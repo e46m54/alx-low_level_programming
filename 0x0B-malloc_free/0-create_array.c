e9019d4941449b9f44974e1a707d02e94a41f91e
e46m54 <mylgv40b@gmail.com>
2022-06-27 22:04:40 +0200

    001

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * @c: storaged char
  * Return: pointer of an array of chars
  */
-har *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *cr;
 	unsigned int i;
