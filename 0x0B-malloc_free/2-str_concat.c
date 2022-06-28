5fa88494467efab13fbf740e01da70f9ac3ef2ad
e46m54 <mylgv40b@gmail.com>
2022-06-28 16:42:34 +0200

    03

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,13 +26,13 @@ char *str_concat(char *s1, char *s2)
 	if (strout == NULL)
 	{
 		free(strout);
-		return (NULL);
+		return (0);
 	}
 
 	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
 
-	li = j;
+	lim = j;
 	for (j = 0; j <= lim; k++, j++)
 		strout[k] = s2[j];
 
