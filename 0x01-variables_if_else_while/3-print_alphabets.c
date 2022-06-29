ca6e3322a06f9dc74a9a28d3a3eebd3965a4455d
e46m54 <mylgv40b@gmail.com>
2022-06-29 13:41:35 +0200

    try

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - print alpha a-z A-Z
+*Return: 0
+**/
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+
+	{
+		putchar('\n');
+	}
+
+	Return(0);
+
+}
